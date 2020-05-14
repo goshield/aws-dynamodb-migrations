dd407c75134745b3d1c237e1a98d4103e2f7a93e
Lucas <me@dotronglong.com>
2020-05-15 00:09:48 +0800

    Add migrator

---
+++ migrator.go
@@ -0,0 +1,29 @@
+package aws_dynamodb_migrations
+
+import (
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/goshield/interfaces"
+)
+
+// NewMigrator returns a migrator for DynamoDB
+func NewMigrator(db *dynamodb.DynamoDB, paths []string) interfaces.Migrator {
+	return &dynamoDBMigrator{
+		db:    db,
+		paths: paths,
+	}
+}
+
+type dynamoDBMigrator struct {
+	paths []string
+	db    *dynamodb.DynamoDB
+}
+
+func (m dynamoDBMigrator) Migrate() {
+	im := NewImporter(m.db)
+	for _, path := range m.paths {
+		err := im.Import(path)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
